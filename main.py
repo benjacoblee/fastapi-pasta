584eeefffe483aedcaa9cb1297a3030581f94ea8
benjacoblee <benjacoblee@gmail.com>
2024-01-16 11:25:30 +0800

    integrate sqlite

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import os
 from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from sqlalchemy import create_engine, String
+from sqlalchemy.orm import (
+    sessionmaker,
+    Session,
+    DeclarativeBase,
+    Mapped,
+    mapped_column,
+)
 from pydantic import BaseModel
 from datetime import datetime, timedelta
 from jose import JWTError, jwt
 from passlib.context import CryptContext
 from dotenv import load_dotenv
-from fake_db import db
 
 load_dotenv()
 
@@ -17,6 +24,7 @@ if not SECRET_KEY:
 
 ALGORITHM = os.getenv("ALGORITHM") or "HS256"
 ACCESS_TOKEN_EXP_MINUTES = os.getenv("ACCESS_TOKEN_EXP_MINUTES") or 30
+DATABASE_URL = "sqlite:///test.db"
 
 
 class Token(BaseModel):
@@ -30,18 +38,54 @@ class TokenData(BaseModel):
 
 class User(BaseModel):
     username: str | None = None
-    full_name: str | None = None
-    disabled: bool | None = None
 
 
 class UserInDB(User):
     hashed_password: str
 
 
+class Base(DeclarativeBase):
+    pass
+
+
+class DBItem(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    username: Mapped[str] = mapped_column(String(30))
+    hashed_password: Mapped[str] = mapped_column(String(30))
+
+
+class DBUsername:
+    def __init__(self, username: str):
+        self.username = username
+
+
+class Item(BaseModel):
+    id: int
+    username: str
+    hashed_password: str
+
+
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth_2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 app = FastAPI()
+
+
+def get_db():
+    database = SessionLocal()
+    try:
+        yield database
+    finally:
+        database.close()
+
+
+@app.on_event("startup")
+async def startup():
+    Base.metadata.create_all(bind=engine)
 
 
 def verify_pw(plain_pw, hashed_pw):
@@ -52,14 +96,14 @@ def get_pw_hash(pw):
     return pwd_context.hash(pw)
 
 
-def get_user(db, username: str):
-    if username in db:
-        user_data = db[username]
-        return UserInDB(**user_data)
+async def get_user(username: str, db: Session):
+    db_item = db.query(DBItem).filter(DBItem.username == username).first()
+    if db_item:
+        return Item(**db_item.__dict__)
 
 
-def auth_user(db, username: str, pw: str):
-    user = get_user(db, username)
+async def auth_user(username: str, pw: str, db: Session):
+    user = await get_user(username, db)
     if not user:
         return False
     if not verify_pw(pw, user.hashed_password):
@@ -79,7 +123,9 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
     return encoded_jwt
 
 
-async def get_current_user(token: str = Depends(oauth_2_scheme)):
+async def get_current_user(
+    token: str = Depends(oauth_2_scheme), db: Session = Depends(get_db)
+):
     credential_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
@@ -90,21 +136,21 @@ async def get_current_user(token: str = Depends(oauth_2_scheme)):
         username = payload.get("sub")
         if username is None:
             raise credential_exception
-        user = get_user(db, username)
+        user = await get_user(username, db)
         return user
     except JWTError:
         raise credential_exception
 
 
 async def get_current_active_user(current_user: UserInDB = Depends(get_current_user)):
-    if current_user.disabled:
-        raise HTTPException(status_code=400, detail="Inactive user")
     return current_user
 
 
 @app.post("/token", response_model=Token)
-async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
-    user = auth_user(db, form_data.username, form_data.password)
+async def login_for_access_token(
+    form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)
+):
+    user = await auth_user(form_data.username, form_data.password, db)
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
@@ -125,15 +171,23 @@ async def read_users_me(current_user: User = Depends(get_current_active_user)):
 
 @app.get("/users/me/items")
 async def read_own_items(current_user: User = Depends(get_current_active_user)):
-    return [{"item_id": 1, "owner": current_user}]
+    return [{"item_id": 1, "owner": current_user.username}]
 
 
-@app.post("/register")
-async def register_user(username: str, pw: str):
-    user = get_user(db, username)
+@app.post("/register", response_model=User)
+async def register_user(username: str, pw: str, db: Session = Depends(get_db)):
+    user = await get_user(username, db)
     if user:
         raise HTTPException(400, detail="Username already taken")
     hashed_pw = get_pw_hash(pw)
-    created_user = {"username": username, "hashed_password": hashed_pw}
-    db[username] = created_user
-    return {"status": "success"}
+    db_item = DBItem(username=username, hashed_password=hashed_pw)
+    db.add(db_item)
+    db.commit()
+    db.refresh(db_item)
+    return await get_user(username, db)
+
+
+@app.get("/users")
+async def get_users(db: Session = Depends(get_db)):
+    usernames = [item[0] for item in db.query(DBItem.username).all()]
+    return usernames
