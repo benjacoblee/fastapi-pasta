d6bbdb3fecd7803a765c932db21893420b284f4d
benjacoblee <benjacoblee@gmail.com>
2024-01-16 21:56:47 +0800

    remove unused class

--- main.py
+++ main.py
@@ -46,10 +46,6 @@ class Token(BaseModel):
     token_type: str
 
 
-class TokenData(BaseModel):
-    username: str | None = None
-
-
 class StatusDetail(BaseModel):
     status_code: int
     detail: str
